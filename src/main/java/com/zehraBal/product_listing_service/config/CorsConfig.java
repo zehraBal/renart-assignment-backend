c455ffe57b753a28041591e7d5fe82a1d98813a5
zehraBal <fz.bal00@gmail.com>
2025-07-08 12:58:29 +0300

    Add CORS configuration to allow requests from frontend

---
+++ src/main/java/com/zehraBal/product_listing_service/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.zehraBal.product_listing_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") //Todo: Change when you deploy frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
