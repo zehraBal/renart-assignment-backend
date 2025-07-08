ad88011ffe4c0bfca32b610e86b3944971c9bec9
zehraBal <fz.bal00@gmail.com>
2025-07-08 04:28:20 +0300

    initial commit

---
+++ src/main/java/com/zehraBal/product_listing_service/ProductListingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zehraBal.product_listing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductListingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductListingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zehraBal/product_listing_service/ProductListingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zehraBal.product_listing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductListingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
